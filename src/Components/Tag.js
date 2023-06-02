288127bcabdecefecb4f86bab2983409ecc75bef
monson2002 <monsonreji2002@gmail.com>
2023-06-02 19:19:30 +0530

    Added tailwind classes

--- src/Components/Tag.js
+++ src/Components/Tag.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Tag({ tagname }) {
     return (
         <>
-            <div className="tag">{tagname}</div>;
+            <div className="tag h-6 w-10 my-1 mx-2 p-1 bg-orange-500 text-xs text-white rounded-2xl font-Roboto text-center">{tagname}</div>
         </>
     );
 }
